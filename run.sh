c0d8c0ca4458bb7726a8af29fbb07b43797e707a
Brian Henning <henninb@msn.com>
2025-09-02 06:52:42 -0500

    wip

--- run.sh
+++ run.sh
@@ -7,13 +7,9 @@ echo python unsubscribe_and_block.py --domain info.curaleaf.com --execute --forc
 echo python unsubscribe_and_block.py --domain t.timberland.com --execute
 echo python unsubscribe_and_block.py --domain email.totaltools.com.au --execute
 echo python unsubscribe_and_block.py --domain trulieve.com --execute
-echo python real_demo.py --auth
 echo python -m inbox_cleaner.cli auth
 echo python unsubscribe_and_block.py --all-domains --dry-run
-echo python quick_cleanup.py --archive-old-promos --dry-run
-echo python real_demo.py --stats
 echo python -m inbox_cleaner.cli
-echo python real_demo.py --extract 4225
-echo python real_demo.py --extract 500
 echo pytest
+
 exit 0
